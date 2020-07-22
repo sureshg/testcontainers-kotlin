332a57cadc0099e371900de3f0f61fe7e51b757b
Suresh G <sureshg.g@gmail.com>
2020-07-21 22:55:00 -0700

    feat: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "dev.suresh"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(enforcedPlatform("org.junit:junit-bom:5.6.2"))
+    testImplementation(enforcedPlatform("org.testcontainers:testcontainers-bom:1.15.0-rc1"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:cassandra")
+}
---
+++ src/test/kotlin/dev/suresh/CassandraTest.kt
@@ -0,0 +1,38 @@
+package dev.suresh
+
+import com.datastax.driver.core.*
+import org.junit.*
+import org.junit.Assert.assertEquals
+import org.testcontainers.containers.*
+import org.testcontainers.junit.jupiter.*
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.utility.*
+
+
+@Testcontainers
+class CassandraTest {
+
+    @Container
+    val cassandra = CassandraContainer<Nothing>(DockerImageName.parse("cassandra:latest"))
+        .apply { start() }
+
+    @Test
+    fun `Test Cassandra connection`() {
+        val cluster = cassandra.cluster
+        println(cluster)
+        cluster.connect().use { session ->
+            session.execute(
+                """
+                    CREATE KEYSPACE IF NOT EXISTS test WITH replication = 
+                    {'class':'SimpleStrategy','replication_factor':'1'};
+                    """.trimIndent()
+            )
+            val keySpaces = session.cluster
+                .metadata
+                .keyspaces
+                .filter { km: KeyspaceMetadata -> km.name == "test" }
+            println(keySpaces)
+            assertEquals(1, keySpaces.size)
+        }
+    }
+}
