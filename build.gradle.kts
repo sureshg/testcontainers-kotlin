2d36beba4134d7c84d379c8d25ef4dd925d8671c
Suresh G <sureshg.g@gmail.com>
2020-07-22 02:56:25 -0700

    format: build.gradle.kts formatting changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,12 @@ tasks {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
     testImplementation(enforcedPlatform("org.junit:junit-bom:5.6.2"))
     testImplementation(enforcedPlatform("org.testcontainers:testcontainers-bom:1.15.0-rc1"))
 
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.testcontainers:testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers")
     testImplementation("org.testcontainers:cassandra")
 }
